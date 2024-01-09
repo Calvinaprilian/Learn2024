43fa15c3dd477bf5def58169bac9ac0d321a3c4f
Yusuf Calvin Aprilian <yusufcalvin998@gmail.com>
2024-01-09 10:49:53 +0700

    merevisi perkalian

--- fitur.js
+++ fitur.js
@@ -4,8 +4,9 @@ console.log(add(1,2));
 const subtract = (a, b) => a - b;
 console.log(subtract(5,3));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(3,5));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(3,5));
 
 const divide = (a, b) => a / b;
 console.log(divide(8,2));
+
